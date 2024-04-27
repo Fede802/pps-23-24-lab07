8a415bb521a894be772e17e4f82655bda9bd0fdf
Federico Bravetti <federico.bravetti2@studio.unibo.it>
2024-04-27 13:08:26 +0200

    new robots implemented

--- src/main/scala/ex2/Robot.scala
+++ src/main/scala/ex2/Robot.scala
@@ -42,9 +42,66 @@ class LoggingRobot(val robot: Robot) extends Robot:
     robot.act()
     println(robot.toString)
 
+class RobotWithBattery(val robot: Robot, private var _batteryUsage: Int = 0, private var _batteryLevel: Int = 0) extends Robot:
+  export robot.{position, direction}
+
+  private def charged: Boolean =
+    _batteryLevel >= _batteryUsage
+
+  private def discharge(): Unit =
+    _batteryLevel -= _batteryUsage
+
+  def batteryUsage: Int = _batteryUsage
+
+  def batteryLevel: Int = _batteryLevel
+
+  override def turn(dir: Direction): Unit =
+    if charged then
+      robot.turn(dir)
+      discharge()
+
+  override def act(): Unit =
+    if charged then
+      robot.act()
+      discharge()
+
+class RobotCanFail(val robot: Robot, val failProbability: Double = 0, val seed: Int = 1234) extends Robot:
+  export robot.{position, direction}
+  import scala.util.Random
+  private val random = Random(seed)
+  private def available: Boolean =
+    random.nextDouble()*100 > failProbability
+
+  override def turn(dir: Direction): Unit =
+    if available then
+      robot.turn(dir)
+
+  override def act(): Unit =
+    if available then
+      robot.act()
+
+class RobotRepeated(val robot: Robot, val numberOfRepetition: Int = 0) extends Robot:
+  export robot.{position, direction}
+
+  override def turn(dir: Direction): Unit =
+    for
+      _ <- 0 until numberOfRepetition
+    yield robot.turn(dir)
+
+  override def act(): Unit =
+    for
+      _ <- 0 until numberOfRepetition
+    yield robot.act()
+
+
 @main def testRobot(): Unit =
   val robot = LoggingRobot(SimpleRobot((0, 0), Direction.North))
   robot.act() // robot at (0, 1) facing North
   robot.turn(robot.direction.turnRight) // robot at (0, 1) facing East
   robot.act() // robot at (1, 1) facing East
   robot.act() // robot at (2, 1) facing East
+  val robot2 = RobotWithBattery(SimpleRobot((0, 0), Direction.North),10)
+  robot2.act()
+  val robot3 = RobotRepeated(SimpleRobot((0, 0), Direction.North),3)
+  robot3.act()
+  println(robot3.position)
